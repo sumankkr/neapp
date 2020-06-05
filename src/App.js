fcf707fdf0f54e6ccbcf6c3589d7b3f891614529
Biswa Sarkar <biswabrata77@gmail.com>
2020-06-05 21:54:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container" style={{margin:'150px auto'}}>
+       <div className="col-sm-5 text-center speed_container">
+         <div className="panel panel-default">
+         <button id='startbutton'>
+       Start Test</button>
+    status: <span id='status'>
+   </span>
+  <div className="panel-body text-center">
+   Download Speed <h3 id="down">0</h3>
+        Upload Speed <h3 id="up">0</h3>
+        Ping <h3 id="ping">0</h3>
+        Jitter <h3 id="jitter">0</h3>
+        IP <h4 id="ip">0</h4>
+        Server <h4 id="nearest">0</h4>
+         Quality and Buffert bolt <h4 id="grades">0</h4>
+           DNS <h4 id="dns">0</h4>
+        <div id='logdiv' style={{display:'none'}}>
+ </div>
+           </div>
+         </div>
+       </div>
+   </div>
+  
+  
+     </div>
   );
 }
 
